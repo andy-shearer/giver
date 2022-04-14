2e316c6b5ad362f747e97c6b64cfe916c6660f18
Andy Shearer <andyshearer.95@googlemail.com>
2022-04-14 17:04:22 +0100

    Add basic wallet connection bits

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,10 +1,37 @@
-import Head from "next/head"
-import Image from "next/image"
 import styles from "../styles/Home.module.css"
-import Navbar from "../components/navbar"
+import Navbar from "../components/navbar"j
 import InfoBubble from "../components/infobubble"
+import { useState, useEffect, userRef } from "react";
+import Web3Modal from "web3modal";
+import { Contract, providers } from "ethers";
+import { GIVER_CONTRACT_ADDR, GIVER_CONTRACT_ABI } from "../constants"
 
 export default function Home() {
+  const { walletConnected, setWalletConnected } = useState(false);
+  const web3ModalRef = useRef();
+
+  // Prompt for the wallet to be connected if it is not connected.
+  const connectWallet = () => {
+    if(!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "mumbai",
+        disableInjectedProvider: false,
+        providerOptions: {}
+      });
+
+      const instance = await web3ModalRef.current.connect();
+      const provider = new providers.Web3Provider(instance);
+
+      const { chainId } = await web3Provider.getNetwork();
+      if (chainId !== 80001) {
+        window.alert("Please switch to the Mumbai test network!");
+        throw new Error("Please switch to the Mumbai test network");
+      }
+
+      const signer = provider.getSigner();
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Navbar />
